be3375d1623c1da6e41e75506cd4053b69277641
Matt Butrovich <mbutrovich@gmail.com>
2021-04-09 11:08:36 -0400

    Reorder operations to make sure stuff gets flushed regardless of sampling rate. Stash incomplete read metrics.

--- src/network/connection_handle.cpp
+++ src/network/connection_handle.cpp
@@ -172,25 +172,36 @@ void ConnectionHandle::HandleEvent(int fd, int16_t flags) {
 }
 
 Transition ConnectionHandle::TryRead() {
+  // TODO(Matt): there's an edge case here: what happens if it takes multiple TryRead's to build a packet? ie the query
+  // data spans multiple packets. The network state machine is tricky
+
+  // check first if we have data to flush
+  if (flush_read_features_) {
+    if (context_.read_features_.num_queries_ == 1) {
+      // only flush metrics if we have a single query. Don't know how to model anything else right now.
+      common::thread_context.metrics_store_->RecordNetworkData(context_.read_features_, context_.read_metrics_);
+    }
+    // reset read metrics and features
+    flush_read_features_ = false;
+    context_.read_features_ = {.operating_unit_ = 1};
+  }
+
+  // sample for a new data point
   const bool do_a_metrics_thing =
       common::thread_context.metrics_store_ != nullptr &&
       common::thread_context.metrics_store_->ComponentToRecord(metrics::MetricsComponent::NETWORK);
   if (do_a_metrics_thing) {
-    if (flush_read_features_) {
-      if (context_.read_features_.num_queries_ == 1) {
-        // only flush metrics if we have a single query. Don't know how to model anything else right now.
-        common::thread_context.metrics_store_->RecordNetworkData(context_.read_features_,
-                                                                 common::thread_context.resource_tracker_.GetMetrics());
-      }
-      flush_read_features_ = false;
-      context_.read_features_ = {.operating_unit_ = 1};
-    }
+    // perform read while profiling
     common::thread_context.resource_tracker_.Start();
     const auto read_transition = io_wrapper_->FillReadBuffer();
     common::thread_context.resource_tracker_.Stop();
+    // stash features and metrics in context
     context_.read_features_.bytes_ = io_wrapper_->GetReadBuffer()->Size();
     common::thread_context.resource_tracker_.SetNetworkRead(context_.read_features_.bytes_);
+    context_.read_metrics_ = common::thread_context.resource_tracker_.GetMetrics();
+    // flush at next opportunity
     flush_read_features_ = true;
+    // return result of profiled op
     return read_transition;
   }
   return io_wrapper_->FillReadBuffer();
@@ -198,19 +209,23 @@ Transition ConnectionHandle::TryRead() {
 
 Transition ConnectionHandle::TryWrite() {
   if (io_wrapper_->ShouldFlush()) {
+    // check first if we have data to flush
+    if (flush_read_features_) {
+      if (context_.read_features_.num_queries_ == 1) {
+        // only flush metrics if we have a single query. Don't know how to model anything else right now.
+        common::thread_context.metrics_store_->RecordNetworkData(context_.read_features_, context_.read_metrics_);
+      }
+      // reset read metrics and features
+      flush_read_features_ = false;
+      context_.read_features_ = {.operating_unit_ = 1};
+    }
+
+    // sample for a new data point
     const bool do_a_metrics_thing =
         common::thread_context.metrics_store_ != nullptr &&
         common::thread_context.metrics_store_->ComponentToRecord(metrics::MetricsComponent::NETWORK);
     if (do_a_metrics_thing) {
-      if (flush_read_features_) {
-        if (context_.read_features_.num_queries_ == 1) {
-          // only flush metrics if we have a single query. Don't know how to model anything else right now.
-          common::thread_context.metrics_store_->RecordNetworkData(
-              context_.read_features_, common::thread_context.resource_tracker_.GetMetrics());
-        }
-        flush_read_features_ = false;
-        context_.read_features_ = {.operating_unit_ = 1};
-      }
+      // perform write while profiling
       context_.write_features_.bytes_ = io_wrapper_->GetWriteQueue()->Size();
       common::thread_context.resource_tracker_.Start();
       const auto write_transition = io_wrapper_->FlushAllWrites();
@@ -222,6 +237,7 @@ Transition ConnectionHandle::TryWrite() {
                                                                  common::thread_context.resource_tracker_.GetMetrics());
       }
       context_.write_features_ = {.operating_unit_ = 2};
+      // return result of profiled op
       return write_transition;
     }
     return io_wrapper_->FlushAllWrites();
