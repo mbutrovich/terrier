fe58f6de4e0f0f054a15ecc40c11a626fcf2a967
Matt Butrovich <mbutrovich@gmail.com>
2021-05-11 16:26:11 -0400

    Refactor.

--- script/self_driving/modeling/data/sample_online_data.py
+++ script/self_driving/modeling/data/sample_online_data.py
@@ -7,21 +7,21 @@ import sys
 
 if __name__ == "__main__":
     try:
-        opts, args = getopt.getopt(sys.argv[1:], "ior:", ["input=", "output=", "rate="])
+        opts, args = getopt.getopt(sys.argv[1:], "ios:", ["input=", "output=", "samples="])
     except getopt.GetoptError as err:
         # print help information and exit:
         print(err)  # will print something like "option -a not recognized"
         sys.exit(2)
     input_file = None
     output_folder = None
-    rate = None
+    samples = None
     for o, a in opts:
         if o in ['-i', '--input']:
             input_file = a
         elif o in ['-o', '--output']:
             output_folder = a
-        elif o in ['-r', '--rate']:
-            rate = int(a)
+        elif o in ['-s', '--samples']:
+            samples = int(a)
         else:
             assert False, "unhandled option"
 
@@ -29,7 +29,7 @@ if __name__ == "__main__":
         print("Need input and output locations and rate.")
         sys.exit(2)
 
-    assert (0 < rate < 100), "Invalid sampling rate."
+    assert (0 < samples), "Invalid sample number."
 
     df = pd.read_csv(input_file)
     df.columns = df.columns.str.strip()
@@ -37,5 +37,5 @@ if __name__ == "__main__":
     filename = os.path.basename(input_file)
     (file, ext) = os.path.splitext(filename)
 
-    df = df.sample(frac=rate / 100, random_state=1)
-    df.to_csv("{}/{}_{}.csv".format(output_folder, file, rate), index=False)
+    df = df.sample(n=samples, random_state=1)
+    df.to_csv("{}/{}.csv".format(output_folder, file), index=False)
